f8d5bc63e8218ae38b839b8189fd32870032bdda
saishkulkarni <saishkulkarni7@Gmail.com>
2026-01-03 17:03:15 +0530

    4. Jwt Filter and Service Added

--- src/main/java/com/jsp/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/jsp/ecommerce/config/SecurityConfig.java
@@ -5,13 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.jsp.ecommerce.security.JwtFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final JwtFilter jwtFilter;
 
 	@Bean
 	PasswordEncoder encoder() {
@@ -21,6 +30,8 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain chain(HttpSecurity httpSecurity) {
 		return httpSecurity.httpBasic(Customizer.withDefaults())
-				.authorizeHttpRequests(x -> x.anyRequest().authenticated()).build();
+				.authorizeHttpRequests(x -> x.requestMatchers("/auth/**").permitAll().anyRequest().authenticated())
+				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+				.sessionManagement(x -> x.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
 	}
 }
--- src/main/java/com/jsp/ecommerce/controller/AuthController.java
+++ src/main/java/com/jsp/ecommerce/controller/AuthController.java
@@ -1,22 +1,10 @@
 package com.jsp.ecommerce.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
-
-	@GetMapping("/check")
-	@PreAuthorize("hasRole('ADMIN')")
-	public String check() {
-		return "Checked and Working !!!";
-	}
-
-	@GetMapping("/test")
-	@PreAuthorize("hasRole('USER')")
-	public String test() {
-		return "Tested and Working !!!";
-	}
 
 }
